6b7279f8c24004cbbdbf7c23c6809456030d776b
llsanketll <sanket.lamsal@gmail.com>
2022-12-15 17:11:34 +0545

    Implement Jumping

--- EndlessRunner/EndlessRunner.cpp
+++ EndlessRunner/EndlessRunner.cpp
@@ -3,15 +3,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "ShaderClass.h"
 #include "VertexArrayObject.h"
 #include "VertexBufferObject.h"
 #include "ElementBufferObject.h"
 
+int width = 800, height = 800;
+float Normalize(float input, float min, float max)
+{
+	return (input - min) / (max - min);
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+
+void TakeInput(GLFWwindow* window, float& x, float& y)
+{
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		x += 1;
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		x -= 1;
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		y += 1;
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		y -= 1;
+}
+
 
 int main()
 {
@@ -20,10 +41,13 @@ int main()
 	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Get Screen Resolution
 	GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
 	const GLFWvidmode* screen = glfwGetVideoMode(primaryMonitor);
 	std::cout << screen->width << ", " << screen->height << std::endl;
-	GLFWwindow* window = glfwCreateWindow(screen->width / 2, screen->height, "EndlessRunner", NULL, NULL);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "EndlessRunner", NULL, NULL);
 
 
 	if (window == NULL)
@@ -32,8 +56,7 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window)
-		;
+	glfwMakeContextCurrent(window);
 
 	//Load GLAD so it configures OPENGL
 	gladLoadGL();
@@ -41,17 +64,15 @@ int main()
 
 	//Triangle Vertices
 	GLfloat vertices[] = {
-		0.0f, 0.0f, 0.0f,
-		 0.5f, 0.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
-		 0.0f, 0.45f, 0.0f,
-		 0.45f,  0.45f, 0.0f,
-		 0.0f, 1.0f, 0.0f
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 	};
 
 	GLuint indices[] = {
 		0, 1, 2,
-		3, 4, 5
+		2, 1, 3
 	};
 
 
@@ -69,17 +90,60 @@ int main()
 	EBO1.Unbind();
 
 
+	float lastTime = 0, currentTime, deltaTime;
+	float x = -200.0f, y = 0;
+	float speedX = 0, speedY = 0;
+	float gravity = -1.0f;
+	float cubeScale = 0.25f;
+	float squarePixelSize = cubeScale * 0.5f * height / 2;
+	bool jumping = false;
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		currentTime = static_cast<float>(glfwGetTime());
+		deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
 		shaderProgram.Activate();
+
+		//TakeInput(window, x, y);
+
+		y += speedY;
+		speedY += gravity;
+		//y = -height / 2 + 0.125 * height / 2;
+
+		if (y - squarePixelSize < -height / 2)
+		{
+			y = -height / 2 + squarePixelSize;
+			speedY = 0;
+			jumping = false;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !jumping)
+		{
+			speedY = 20.0f;
+			jumping = true;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+
+		model = glm::translate(model, glm::vec3(Normalize(x, 0, (float)width / 2), Normalize(y, 0, (float)height / 2), 0.0f));
+		model = glm::scale(model, glm::vec3(cubeScale, cubeScale, 1.0f));
+
+		glm::mat4 MVP = projection * view * model;
+		GLuint MVPLoc = glGetUniformLocation(shaderProgram.ID, "MVP");
+		glUniformMatrix4fv(MVPLoc, 1, GL_FALSE, glm::value_ptr(MVP));
+
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 7, GL_UNSIGNED_INT, 0);
-		glfwSwapBuffers(window);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
+
 
 		//Take Care of all glfw Events
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
